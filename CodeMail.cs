23f129b8d1c2839bf36a325ed7f6d8f6a036c103
Student <Student@111-8>
2022-10-19 10:49:07 +0300

    Добавьте файлы проекта.

---
+++ CodeMail.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace Pksrch
+{
+    internal class CodeMail
+    {
+        int code;
+        string mail;
+        public void CodeInit()
+        {
+            Random r = new Random();
+            code = r.Next(100000, 999999);
+                        
+            
+            Console.WriteLine("Введите вашу почту");
+            string mail = Console.ReadLine();
+
+            MailAddress from = new MailAddress("artyom.grechko.dl@yandex.ru", "Артемидзе");
+            MailAddress to = new MailAddress(mail);
+
+            MailMessage message = new MailMessage(from, to);
+            message.Subject = "Код подтверждения";
+            message.Body = "<h4>Данное письмо сгенерировано роботом автоматически. Пожалуйста, не отвечайте на него.</h4>" + "Ваш код подтверждения: " + code.ToString();
+            message.IsBodyHtml = true;
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.yandex.ru";
+            smtp.Port = 587;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;            
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("artyom.grechko.dl@yandex.ru", "c1b5rS1r");
+            smtp.Send(message);
+            
+        }
+
+        public void verify()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите код: ");
+                if (Console.ReadLine() == code.ToString())
+                {
+                    Console.WriteLine("Регистрация завершена");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Введён неправильный код");
+                    verify();
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Pksrch;
+
+namespace NetConsoleApp
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            CodeMail lolo = new CodeMail();
+            lolo.CodeInit();
+            lolo.verify();
+            
+        }
+    }
+}
